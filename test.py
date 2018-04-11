504dc8b791c8034f0130923cff97effc06bdca2b
EleanoreShen <1604627560@qq.com>
2018-04-11 20:20:01 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,4 @@
+transaction = OrderedDict({'sender_address': 'lasnfknf', 
+                                    'drug': 'sakdfnsk',
+                                    'number': 'naskfnd',
+                                    'factory': 'sdfkndakf'})
